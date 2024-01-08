d951fc971b3c32db4f8416e054b6807e997e5d31
tiviku <105354467+filmovie@users.noreply.github.com>
2024-01-08 18:47:53 +0700

    Add files via upload

--- js/createElement.js
+++ js/createElement.js
@@ -6,9 +6,9 @@ document.body.appendChild(scrollmenu);
 /*
 Yowis.Ben.2.(2019).html
 */
-let a124 = document.createElement('span');
-a124.innerHTML = "<a class='film' href=' fvod/Yowis.Ben.2.(2019).html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w220_and_h330_face/t1N4uWXg17fZ5PsqQ0UilKqStBe.jpg ' alt=' Yowis.Ben.2.(2019) '></button></a>";
-scrollmenu.appendChild(a124);
+let a125 = document.createElement('span');
+a125.innerHTML = "<a class='film' href=' fvod/Yowis.Ben.2.(2019).html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w220_and_h330_face/t1N4uWXg17fZ5PsqQ0UilKqStBe.jpg ' alt=' Yowis.Ben.2.(2019) '></button></a>";
+scrollmenu.appendChild(a125);
 
 let a124 = document.createElement('span');
 a124.innerHTML = "<a class='film' href=' fvod/Ford.v.Ferrari.(2019).dubbing.html '><button><img class='gfilm' src=' https://www.themoviedb.org/t/p/w220_and_h330_face/dR1Ju50iudrOh3YgfwkAU1g2HZe.jpg ' alt=' Ford.v.Ferrari.(2019).dubbing '></button></a>";
