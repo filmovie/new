345c0d2d640a4316daa24d7c65ce375cfd7c702d
tiviku <105354467+filmovie@users.noreply.github.com>
2024-01-20 00:19:41 +0700

    Add files via upload

--- js/Squid.Game.(2021).dubbing.js
+++ js/Squid.Game.(2021).dubbing.js
@@ -3,10 +3,12 @@ scrollmenu.align ="center";
 scrollmenu.className = "scrollmenu";
 document.body.appendChild(scrollmenu);
 
+var frm = document.getElementById("gtu");
+
 let form = document.createElement("form");
 form.innerHTML ="<div class='container'><div class='left'><input type='text'name='' id='cari' onkeyup='prosesMenu()' placeholder='search'><br><br></div></div>";
 frm.appendChild(form);
 
 let a1 = document.createElement('span');
-a1.innerHTML = "<a class='film' href=' fvod/e1Squid.Game.(2021).dubbingv.html '><button class='gf'><p><b>eps.01</b></p></button></a>";
+a1.innerHTML = "<a class='film' href=' fvod/1.Captain.Tsubasa.Road.to.(2002).dubbingv.html '><button class='gf'><p><b>eps.01</b></p></button></a>";
 scrollmenu.appendChild(a1);
